3cf00ef897d0a10239b68085c4ac27476ac042b3
TejasS1233 <tejas.sidhwani@gmail.com>
2026-05-16 23:26:07 +0530

    feat: add drift database with Users, Sessions, Trees, Achievements tables

---
+++ focus_app/lib/core/db/database.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+part 'database.g.dart';
+
+class Users extends Table {
+  TextColumn get id => text()();
+  TextColumn get displayName => text().nullable()();
+  IntColumn get streakCount => integer().withDefault(const Constant(0))();
+  IntColumn get longestStreak => integer().withDefault(const Constant(0))();
+  IntColumn get totalFocusSeconds => integer().withDefault(const Constant(0))();
+  DateTimeColumn get createdAt => dateTime()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class Sessions extends Table {
+  TextColumn get id => text()();
+  TextColumn get userId => text()();
+  TextColumn get mode => text()();
+  DateTimeColumn get startTime => dateTime()();
+  DateTimeColumn get endTime => dateTime().nullable()();
+  IntColumn get durationSeconds => integer().withDefault(const Constant(0))();
+  TextColumn get intention => text().nullable()();
+  TextColumn get ambientSound => text().nullable()();
+  TextColumn get outcome => text()();
+  TextColumn get roomId => text().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class Trees extends Table {
+  TextColumn get id => text()();
+  TextColumn get userId => text()();
+  TextColumn get sessionId => text().nullable()();
+  TextColumn get species => text()();
+  IntColumn get growthStage => integer().withDefault(const Constant(0))();
+  BoolColumn get isAlive => boolean().withDefault(const Constant(true))();
+  DateTimeColumn get plantedAt => dateTime()();
+  DateTimeColumn get diedAt => dateTime().nullable()();
+  DateTimeColumn get lastWateredAt => dateTime()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class Achievements extends Table {
+  TextColumn get id => text()();
+  TextColumn get userId => text()();
+  TextColumn get key => text()();
+  TextColumn get title => text()();
+  TextColumn get description => text()();
+  DateTimeColumn get unlockedAt => dateTime().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+@DriftDatabase(
+  tables: [Users, Sessions, Trees, Achievements],
+)
+class AppDatabase extends _$AppDatabase {
+  AppDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+
+  Future<Session?> getSessionById(String id) {
+    return (select(sessions)..where((s) => s.id.equals(id))).getSingleOrNull();
+  }
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dir = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dir.path, 'focus_app.db'));
+    return NativeDatabase(file);
+  });
+}
